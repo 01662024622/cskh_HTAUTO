fc3963e4a5ecd15eebd1aa4570e69f366f897339
thangvm <vuthang.hustit@gmail.com>
2022-11-02 00:37:50 +0700

    thangvm

--- routes/api.php
+++ routes/api.php
@@ -23,4 +23,6 @@ use Illuminate\Support\Facades\Route;
 // });
 Route::group(['prefix' => 'v1'], function() {
     Route::get('insurance/table', 'DataBaseApi\DataTableController@insurance')->name('insurance.api.data');
+
+    Route::post('webhook/nhanh/erp', 'DataBaseApi\Webhook@inventory19021996')->name('webhook.api.nhanh');
 });
